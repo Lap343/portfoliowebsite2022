02e0e3814c98582df826d5e5cf01ae9343feffa1
Luis Perez <44172765+Lap343@users.noreply.github.com>
2022-02-04 14:10:32 -0800

    feat(folder-files): give the folder files an opening animation

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,12 +3,16 @@ import styled from "styled-components";
 import { ContentInfoContainer } from "./About";
 
 const Contact = () => {
+    let fileOrder = 3
+
     const [dblClicked, setDblClicked] = useState<boolean>(false)
 
     return(
-        <ContactContainer onClick={(e) => e.detail === 2 && setDblClicked(!dblClicked)} >
-            <h2>Contact</h2>
-            {dblClicked && <ContentInfoContainer>
+        <>
+            <ContactContainer onClick={(e) => e.detail === 2 && setDblClicked(true)} >
+                <h2>Contact</h2>
+            </ContactContainer>
+            {dblClicked && <ContentInfoContainer top="22em" order={fileOrder}>
                 <h3>Contact me:</h3>
                 <p>
                     -Email: luisperez@mypcand.me<br/>
@@ -17,12 +21,14 @@ const Contact = () => {
                     -Twitter: @lap343<br/>
                     -Raven coordinates/ sky-write over: 36.7378° N, 119.7871° W
                 </p>
+                <div onClick={() => setDblClicked(!dblClicked)} >X</div>
             </ContentInfoContainer>}
-        </ContactContainer>
+        </>
     )
 }
 
 const ContactContainer = styled.div`
+    z-index: 10;
     background-image: url("folder.png");
     background-size: cover;
     background-position: center;
@@ -30,6 +36,11 @@ const ContactContainer = styled.div`
     height: auto;
     padding-top: 2em;
     margin-top: 2em;
+    
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
 
     & h2{
         padding-left: 0.5em;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,24 +3,30 @@ import styled from "styled-components";
 import { ContentInfoContainer } from "./About";
 
 const Projects = () => {
+    let fileOrder = 2
+
     const [dblClicked, setDblClicked] = useState<boolean>(false)
 
     return(
-        <ProjectsContainer onClick={(e) => e.detail === 2 && setDblClicked(!dblClicked)} >
-            <h2>Projects</h2>
-            {dblClicked && <ContentInfoContainer>
+        <>
+            <ProjectsContainer onClick={(e) => e.detail === 2 && setDblClicked(true)} >
+                <h2>Projects</h2>
+            </ProjectsContainer>
+            {dblClicked && <ContentInfoContainer top="14em" order={fileOrder}>
                 <h3>My Projects:</h3>
                 <p>
                     -Websites <br/>
                     -Phone Apps <br/>
                     -Games
                 </p>
+                <div onClick={() => setDblClicked(!dblClicked)} >X</div>
             </ContentInfoContainer>}
-        </ProjectsContainer>
+        </>
     )
 }
 
 const ProjectsContainer = styled.div`
+    z-index: 10;
     background-image: url("folder.png");
     background-size: cover;
     background-position: center;
@@ -28,6 +34,11 @@ const ProjectsContainer = styled.div`
     height: auto;
     padding-top: 2em;
     margin-top: 2em;
+    
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
 
     & h2{
         padding-left: 0.5em;
