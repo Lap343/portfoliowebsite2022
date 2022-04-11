3de26d7564ddabc0a0d8d8c550286549c9a0b6dd
Luis Perez <44172765+Lap343@users.noreply.github.com>
2022-04-11 15:19:20 -0700

    feat(file-styles): change the file info styles

--- src/animation/contentInfoVaviant.ts
+++ src/animation/contentInfoVaviant.ts
@@ -2,7 +2,6 @@ const contentInfoVariant = {
     visible: (custom: number) => ({
         width: ['0em', '10em', '45em'],
         height: ['0.1em', '0.1em', '42em'],
-        padding: ['0em','0em', '0em 1em'],
         left: ['10em', `${10 + (custom * 5)}em`, `${15 + (custom * 5)}em`],
         transition: { duration: 1.5 }
     }),
